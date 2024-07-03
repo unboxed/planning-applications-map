17bfd656ce7b6789429619ff8192b20d2c2ddc21
HonTastic2 <2579978o@student.gla.ac.uk>
2024-07-03 12:06:40 +0100

    Added GET method and partially parses information using async function - Needs to be made GeoJSON format

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 import './govuk-styles.scss';
 import axios from 'axios';
 import { DivIcon } from 'leaflet';
+import { parse } from 'nth-check';
 // import data from './london-spots.json';
+var applicationData = {};
+let parsed = false;
 
 
 // Current location finder
@@ -64,38 +67,50 @@ function LocationMarker() {
 
 // API fetch
 axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://southwark.bops-staging.services';
-axios
-  .get('/api/v2/public/planning_applications/search', {
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  })
-  .then((response) => {
-    parseJSON(response);
-    console.log(response)
-  })
-  .catch((e) => {
-    console.log(e);
-  });
 
-
-// Parsing data acquired from GET request
-var applicationData = {};
-
-function parseJSON (data) {
-  for (let i = 0; i < 10; i++) {
-    var currentApplication = data["data"]["data"][i.toString()];
-    applicationData[i.toString()] = {};
-    applicationData[i.toString()]["title"] = currentApplication["property"]["address"]["singleLine"];
-    applicationData[i.toString()]["latitude"] = currentApplication["property"]["address"]["latitude"];
-    applicationData[i.toString()]["longitude"] = currentApplication["property"]["address"]["longitude"];
-    applicationData[i.toString()]["status"] = currentApplication["application"]["status"];
-    applicationData[i.toString()]["description"] = currentApplication["proposal"]["description"];
-    
-  }
+async function fetchData() {
+  const response = await axios.get('/api/v2/public/planning_applications/search', {headers: { 'Access-Control-Allow-Origin': '*' }})
+  .then((response) => response)
+  .catch((e) => {console.log(e);});
+  return response; 
 }
 
+applicationData = parseJSON(await fetchData());
 console.log(applicationData);
+
+
+var data = {
+  "name":"NewFeatureType",
+  "type":"FeatureCollection",
+  "features":[{
+    "type":"Feature",
+    "geometry":{
+      "type":"Marker",
+      "coordinates":[]
+    },
+    "properties":null
+  }]
+};
+
+// Parsing data acquired from GET request
+function parseJSON (data) {
+  var result = {};
+  for (let i = 0; i < 10; i++) {
+    var currentApplication = data.data.data[i.toString()];
+    result[i.toString()] = {
+      "title" : currentApplication["property"]["address"]["singleLine"],
+      "latitude" : currentApplication["property"]["address"]["latitude"],
+      "longitude" : currentApplication["property"]["address"]["longitude"],
+      "status" : currentApplication["application"]["status"],
+      "description" : currentApplication["proposal"]["description"]
+    }
+  }
+  return result;
+}
+
+// data.features[0].geometry.coordinates.push([applicationData["0"]["latitude"], applicationData["0"]["longitude"]]);
+// console.log(data);
+
 
 function App () {
   
