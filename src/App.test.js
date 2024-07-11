f538bae626c764a10d2123c8c15fc678cf097f07
HonTastic2 <2579978o@student.gla.ac.uk>
2024-07-11 11:43:45 +0100

    Removed unnecessary comments

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,8 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'; 
 import App from './App';
 
-test('Header is shown on site', () => {
+test('Location button is shown on site', () => {
   render(<App />);
-  const headerElement = screen.getByText(/Show My Location/);
-  expect(headerElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/Show My Location/);
+  expect(buttonElement).toBeInTheDocument();
 });
